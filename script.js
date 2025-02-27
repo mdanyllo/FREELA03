059ab02fdafde240c133d452c517a4497c97d5fb
mdanyllo <markusdanyllo07@gmail.com>
2025-02-27 14:54:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+onload = () => {
+    document.body.classList.remove("container");
+  };
+
+  const wrapper = document.querySelector(".wrapper");
+  const openBtn = document.getElementById("openBtn");
+  const closeBtn = document.getElementById("closeBtn");
+  
+  openBtn.addEventListener("click", () => {
+      wrapper.classList.add("open");
+      openBtn.style.display = "none";
+      closeBtn.style.display = "inline-block";
+  });
+  
+  closeBtn.addEventListener("click", () => {
+      wrapper.classList.remove("open");
+      closeBtn.style.display = "none";
+      openBtn.style.display = "inline-block";
+  });
+  
+  
+function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = Math.random() * 2 + 3 + 's';
+    document.body.appendChild(heart);
+    setTimeout(() => heart.remove(), 3000);
+}
+setInterval(createHeart, 500);
+
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+    document.body.appendChild(heart);
+    setTimeout(() => heart.remove(), 5000);
+}
+setInterval(createHeart, 300);
